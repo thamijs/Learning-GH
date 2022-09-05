3490618f86d75ed8673d182c22738b6587d20a6b
Matthijs Duursma <mj.duursma@me.com>
2022-09-05 12:18:16 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Matthijs Duursma on 05/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
